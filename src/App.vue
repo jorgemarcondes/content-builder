dc764f7f7056a587f2767e016394c80882b148d0
jorgemarcondes <jorgealbmar@gmail.com>
2019-06-20 16:47:39 -0300

    Testing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div id="app"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+/* eslint-disable no-console */
+// import { schema } from "prosemirror-schema-basic";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+
+import { baseKeymap } from "prosemirror-commands";
+import { undo, redo, history } from "prosemirror-history";
+import { keymap } from "prosemirror-keymap";
+import { Schema,  } from "prosemirror-model";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {};
+  },
+  mounted() {
+    // const schema = new Schema({
+    //   nodes: {
+    //     doc: { content: "block+" },
+    //     paragraph: { group: "block", content: "text*", marks: "_" },
+    //     heading: { group: "block", content: "text*", marks: "" },
+    //     text: { inline: true }
+    //   },
+    //   marks: {
+    //     strong: {},
+    //     em: {}
+    //   }
+    // });
+    const schema = new Schema({
+      nodes: {
+        doc: { content: "paragraph+" },
+        paragraph: {
+          content: "text*",
+          toDOM(node) {
+            return ["p", 0];
+          }
+        },
+        text: {}
+      }
+    });
+    const state = EditorState.create({
+      schema,
+      plugins: [
+        history(),
+        keymap({ "Mod-z": undo, "Mod-y": redo }),
+        keymap(baseKeymap)
+      ]
+    });
+    // eslint-disable-next-line no-unused-vars
+    const view = new EditorView(document.getElementById("app"), {
+      state
+    });
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
